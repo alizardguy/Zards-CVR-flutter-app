1204e5027324f74a43f855eec80a271733a4c789
alizardguy <42082829+alizardguy@users.noreply.github.com>
2022-04-06 16:56:12 -0700

    cool page swipe

--- zardscvrapp/lib/home.dart
+++ zardscvrapp/lib/home.dart
@@ -13,20 +13,39 @@ class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
   PageController pageController = PageController();
 
+  void onTapped(int index) {
+    //page selector
+    setState(() {
+      _selectedIndex = index;
+      pageController.animateToPage(index,
+          duration: Duration(milliseconds: 300), curve: Curves.ease);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Zard")),
       body: PageView(
         controller: pageController,
+        children: [
+          Container(color: Colors.purple),
+          Container(color: Colors.blue),
+          Container(color: Colors.yellow),
+        ],
       ),
-      bottomNavigationBar:
-          BottomNavigationBar(items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: "Home",
-        ),
-      ]),
+      //bottom bar
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: "users"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "wip"),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Color.fromARGB(255, 185, 108, 199),
+        unselectedItemColor: Colors.grey,
+        onTap: onTapped,
+      ),
     );
   }
 }
