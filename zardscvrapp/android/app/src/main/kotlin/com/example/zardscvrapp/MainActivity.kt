63c35b5231c8f885caf1f6ddfda23cc55d932127
alizardguy <42082829+alizardguy@users.noreply.github.com>
2022-04-06 15:37:29 -0700

    soft reset

--- zardscvrapp/android/app/src/main/kotlin/com/example/zardscvrapp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.zardscvrapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- zardscvrapp/lib/utils/dimensions.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter/material.dart';
-
-const webScreenSize = 600; //when it stops being mobile
