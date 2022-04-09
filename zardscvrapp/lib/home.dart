9e81e7a05744391ef58b25edf833b3d8c5599c89
alizardguy <42082829+alizardguy@users.noreply.github.com>
2022-04-09 13:57:48 -0700

    purple buttons

--- zardscvrapp/lib/home.dart
+++ zardscvrapp/lib/home.dart
@@ -16,14 +16,21 @@ class _HomeScreenState extends State<HomeScreen> {
 //button image feed
   int feedOffsetNumber = 0;
   static const double feedPaddingSpace = 15;
+  final feedScrollControl = ScrollController();
 
 //image holders (very cursed, please fix later)
   String firstDisplayedImage = "https://api.compensationvr.tk/img/113.png";
+  String firstImageText = "@alizard";
   String secondDisplayedImage = "https://api.compensationvr.tk/img/113.png";
+  String secondImageText = "@alizard2";
   String thirdDisplayedImage = "https://api.compensationvr.tk/img/113.png";
+  String thirdImageText = "@alizard3";
   String forthDisplayedImage = "https://api.compensationvr.tk/img/113.png";
+  String forthImageText = "@alizard4";
   String fifthDisplayedImage = "https://api.compensationvr.tk/img/113.png";
+  String fifthImageText = "@alizard5";
   String sixthDisplayedImage = "https://api.compensationvr.tk/img/113.png";
+  String sixthImageText = "@alizard6";
 
 //functions n stuff
   PageController pageController = PageController();
@@ -45,6 +52,12 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
+  //reset scroll
+  void scrollUp() {
+    final double start = 0;
+    feedScrollControl.jumpTo(start);
+  }
+
   void feedLook() {
     try {
       final uri = Uri.parse(
@@ -60,6 +73,8 @@ class _HomeScreenState extends State<HomeScreen> {
           try {
             firstDisplayedImage = "https://api.compensationvr.tk/img/" +
                 parsedImageJson[0]['_id'].toString();
+            firstImageText = "taken by: @" +
+                parsedImageJson[0]['takenBy']['username'].toString();
           } catch (e) {
             firstDisplayedImage = 'https://i.imgur.com/weClCCE.png';
           }
@@ -67,6 +82,8 @@ class _HomeScreenState extends State<HomeScreen> {
           try {
             secondDisplayedImage = "https://api.compensationvr.tk/img/" +
                 parsedImageJson[1]['_id'].toString();
+            secondImageText = "taken by: @" +
+                parsedImageJson[1]['takenBy']['username'].toString();
           } catch (e) {
             secondDisplayedImage = 'https://i.imgur.com/weClCCE.png';
           }
@@ -74,6 +91,8 @@ class _HomeScreenState extends State<HomeScreen> {
           try {
             thirdDisplayedImage = "https://api.compensationvr.tk/img/" +
                 parsedImageJson[2]['_id'].toString();
+            thirdImageText = "taken by: @" +
+                parsedImageJson[2]['takenBy']['username'].toString();
           } catch (e) {
             thirdDisplayedImage = 'https://i.imgur.com/weClCCE.png';
           }
@@ -81,6 +100,8 @@ class _HomeScreenState extends State<HomeScreen> {
           try {
             forthDisplayedImage = "https://api.compensationvr.tk/img/" +
                 parsedImageJson[3]['_id'].toString();
+            forthImageText = "taken by: @" +
+                parsedImageJson[3]['takenBy']['username'].toString();
           } catch (e) {
             forthDisplayedImage = 'https://i.imgur.com/weClCCE.png';
           }
@@ -88,6 +109,8 @@ class _HomeScreenState extends State<HomeScreen> {
           try {
             fifthDisplayedImage = "https://api.compensationvr.tk/img/" +
                 parsedImageJson[4]['_id'].toString();
+            fifthImageText = "taken by: @" +
+                parsedImageJson[4]['takenBy']['username'].toString();
           } catch (e) {
             fifthDisplayedImage = 'https://i.imgur.com/weClCCE.png';
           }
@@ -95,9 +118,12 @@ class _HomeScreenState extends State<HomeScreen> {
           try {
             sixthDisplayedImage = "https://api.compensationvr.tk/img/" +
                 parsedImageJson[5]['_id'].toString();
+            sixthImageText = "taken by: @" +
+                parsedImageJson[5]['takenBy']['username'].toString();
           } catch (e) {
             sixthDisplayedImage = 'https://i.imgur.com/weClCCE.png';
           }
+          scrollUp();
         });
       });
     } catch (e) {
@@ -130,6 +156,7 @@ class _HomeScreenState extends State<HomeScreen> {
         children: [
           //page 1
           SingleChildScrollView(
+            controller: feedScrollControl,
             child: Column(
               children: [
                 Padding(
@@ -137,31 +164,37 @@ class _HomeScreenState extends State<HomeScreen> {
                       const EdgeInsets.symmetric(vertical: feedPaddingSpace),
                   child: Image.network(firstDisplayedImage),
                 ),
+                Text(firstImageText),
                 Padding(
                   padding:
                       const EdgeInsets.symmetric(vertical: feedPaddingSpace),
                   child: Image.network(secondDisplayedImage),
                 ),
+                Text(secondImageText),
                 Padding(
                   padding:
                       const EdgeInsets.symmetric(vertical: feedPaddingSpace),
                   child: Image.network(thirdDisplayedImage),
                 ),
+                Text(thirdImageText),
                 Padding(
                   padding:
                       const EdgeInsets.symmetric(vertical: feedPaddingSpace),
                   child: Image.network(forthDisplayedImage),
                 ),
+                Text(forthImageText),
                 Padding(
                   padding:
                       const EdgeInsets.symmetric(vertical: feedPaddingSpace),
                   child: Image.network(fifthDisplayedImage),
                 ),
+                Text(fifthImageText),
                 Padding(
                   padding:
                       const EdgeInsets.symmetric(vertical: feedPaddingSpace),
                   child: Image.network(sixthDisplayedImage),
                 ),
+                Text(sixthImageText),
                 Center(
                     child: Row(
                         mainAxisAlignment: MainAxisAlignment.center,
@@ -171,19 +204,26 @@ class _HomeScreenState extends State<HomeScreen> {
                           padding: const EdgeInsets.all(16.0),
                           child: ElevatedButton(
                               onPressed: currentImage,
+                              style: ElevatedButton.styleFrom(
+                                  primary: Colors.purple),
                               child: const Text("Latest"))),
                       //back button
                       Padding(
                           padding: const EdgeInsets.all(16.0),
                           child: ElevatedButton(
                               onPressed: previousImage,
+                              style: ElevatedButton.styleFrom(
+                                  primary: Colors.purple),
                               child: const Text("Back"))),
 
                       //next button
                       Padding(
                           padding: const EdgeInsets.all(16.0),
                           child: ElevatedButton(
-                              onPressed: nextImage, child: const Text("Next"))),
+                              onPressed: nextImage,
+                              style: ElevatedButton.styleFrom(
+                                  primary: Colors.purple),
+                              child: const Text("Next"))),
                     ]))
               ],
             ),
